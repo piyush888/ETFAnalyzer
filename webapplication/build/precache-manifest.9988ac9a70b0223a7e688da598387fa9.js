223f962c1ed33a38ccf0b7d248fb5a0af92c33d4
piyush888 <piyush888@gmail.com>
2020-07-01 16:09:36 +0530

    build folder

---
+++ webapplication/build/precache-manifest.9988ac9a70b0223a7e688da598387fa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bc81f4677411f3ea716512356145b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa66aaca1dbb3943582c",
+    "url": "/static/css/2.b6e90e4b.chunk.css"
+  },
+  {
+    "revision": "3a2456b357f5740c2997",
+    "url": "/static/css/main.837c2014.chunk.css"
+  },
+  {
+    "revision": "aa66aaca1dbb3943582c",
+    "url": "/static/js/2.d05bc458.chunk.js"
+  },
+  {
+    "revision": "437798c3ad56ffec1176d8df890d9bc3",
+    "url": "/static/js/2.d05bc458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2456b357f5740c2997",
+    "url": "/static/js/main.81a7a908.chunk.js"
+  },
+  {
+    "revision": "3a7ef01bcb4f9f0c1116",
+    "url": "/static/js/runtime-main.36dc832c.js"
+  }
+]);
