baba70be4356d0e6a60c804eb29af9c66aedbcfc
piyush888 <piyush888@gmail.com>
2020-07-02 14:41:36 +0530

    reuploading build and removing build from gitignore

---
+++ webapplication/build/precache-manifest.62a324c204cf11dd1c4b2464a0420d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1bc0bc6d18a54c2f3e05a546b3a8794",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b100cf3ef772ea536f5",
+    "url": "/static/css/2.b6e90e4b.chunk.css"
+  },
+  {
+    "revision": "71dfea322acc4dc76488",
+    "url": "/static/css/main.2c71dc36.chunk.css"
+  },
+  {
+    "revision": "0b100cf3ef772ea536f5",
+    "url": "/static/js/2.b01a8f09.chunk.js"
+  },
+  {
+    "revision": "437798c3ad56ffec1176d8df890d9bc3",
+    "url": "/static/js/2.b01a8f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71dfea322acc4dc76488",
+    "url": "/static/js/main.ee357720.chunk.js"
+  },
+  {
+    "revision": "3a7ef01bcb4f9f0c1116",
+    "url": "/static/js/runtime-main.36dc832c.js"
+  }
+]);
